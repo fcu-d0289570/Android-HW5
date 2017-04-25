3eb1494f6ef49ed9c19d44ce5cdc649a15dd3ed1
fcu-d0289570 <jacky589589@hotmail.com>
2017-04-25 21:10:48 +0800

    你好嗎

---
+++ app/src/main/java/Playoffs.java
@@ -0,0 +1,20 @@
+/**
+ * Created by user on 2017/4/25.
+ */
+
+public class Playoffs {
+    int hostImg;
+    String hostName;
+    String hostScore;
+    int guestImg;
+    String guestName;
+    String guestScore;
+    public Playoffs(int hostImg, String hostName, String hostScore, int guestImg, String guestName, String guestScore){
+        this.hostImg = hostImg;
+        this.hostName = hostName;
+        this.hostScore = hostScore;
+        this.guestImg = guestImg;
+        this.guestName = guestName;
+        this.guestScore = guestScore;
+    }
+}
